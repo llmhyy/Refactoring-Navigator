8f1309aa005d3b07233b6a2e6c71fda2f57df15b
linyun <llmhyy@gmail.com>
2013-12-29 11:07:52 +0800

    Construct a general framework for auto-mapping and refactoring recommendation.

---
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/RecommendRefactoringAction.java
@@ -0,0 +1,35 @@
+package reflexactoring.diagram.action;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class RecommendRefactoringAction implements
+		IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
