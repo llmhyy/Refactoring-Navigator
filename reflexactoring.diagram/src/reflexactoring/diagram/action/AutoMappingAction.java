8a95d3eb5c2dbee105392aee2c81c2ac0a1db7d7
linyun <llmhyy@gmail.com>
2013-12-29 18:34:36 +0800

    Fetch the elements on GMF diagram

--- reflexactoring.diagram/src/reflexactoring/diagram/action/AutoMappingAction.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/AutoMappingAction.java
@@ -1,17 +1,76 @@
 package reflexactoring.diagram.action;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.emf.core.GMFEditingDomainFactory;
+import org.eclipse.gmf.runtime.notation.impl.DiagramImpl;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PartInitException;
+
+import reflexactoring.Module;
+import reflexactoring.Reflexactoring;
+import reflexactoring.diagram.part.ReflexactoringDiagramEditorUtil;
+import reflexactoring.diagram.util.ReflexactoringUtil;
+import reflexactoring.diagram.util.Settings;
 
 public class AutoMappingAction implements IWorkbenchWindowActionDelegate {
 
+	private String diagramName = "/refactoring/default.reflexactoring";
+	
 	@Override
 	public void run(IAction action) {
-		// TODO Auto-generated method stub
+		try {
+			ArrayList<Module> moduleList = getModuleList();
+			ArrayList<ICompilationUnit> compilationUnit = Settings.scope.getScopeCompilationUnitList();
+			
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
+	
+	private ArrayList<Module> getModuleList() throws PartInitException{
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(ReflexactoringUtil.getTargetProjectName());
+		
+		if(project != null){
+			IFile file = project.getFile(diagramName);
+			if(file.exists()){
+				IPath path = file.getFullPath();
+				TransactionalEditingDomain editingDomain = GMFEditingDomainFactory.INSTANCE.createEditingDomain();
+				
+				URI targetURI = URI.createFileURI(path.toFile().toString());
+				ResourceSet resourceSet = editingDomain.getResourceSet();
+				Resource diagramResource = resourceSet.getResource(targetURI, true);
+				
+				Reflexactoring reflexactoring = (Reflexactoring)diagramResource.getContents().get(0);
+				
+				ArrayList<Module> moduleList = new ArrayList<>();
+				for(Module module: reflexactoring.getModules()){
+					moduleList.add(module);
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	
 
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
