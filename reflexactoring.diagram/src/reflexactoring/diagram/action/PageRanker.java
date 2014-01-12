cb4df2b03951d7c4835b52ad9b2c80cd1939e6b4
linyun <llmhyy@gmail.com>
2014-01-12 22:42:33 +0800

    Make the technique of calling matlab from java work. I just write it on test projects. What remains should be how to transplant the plugin to the machines where no matlab installed. Currently, I will focus on some high-level concepts before programming.

--- reflexactoring.diagram/src/reflexactoring/diagram/action/PageRanker.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/PageRanker.java
@@ -12,6 +12,21 @@ import reflexactoring.diagram.bean.ICompilationUnitWrapper;
  *
  */
 public class PageRanker {
+	/**
+	 * This method could spread the weight (similarity) amongst different unit.
+	 * For example, there is a concept named as "draw", we can match the java class
+	 * "DrawFigure" is possibly relevant, however, the class "Canvas" may be regarded
+	 * as irrelevant because there is no keyword shared between "draw" and "canvas".
+	 * Therefore, I intend to spread the similarity if there is a dependency between
+	 * class "Canvas" and "DrawFigure". The spreading algorithm is Page Ranking algorithm.
+	 * 
+	 * The initialVector is the initial similarity between a module and many compilation units.
+	 * 
+	 * 
+	 * @param initialVector
+	 * @param compilationUnitList
+	 * @return
+	 */
 	public double[] generateResultVector(double[] initialVector, 
 			ArrayList<ICompilationUnitWrapper> compilationUnitList){
 		
---
+++ reflexactoring.diagram/test/reflexactoring/diagram/util/test/PageRankingTest.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package reflexactoring.diagram.util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.mathworks.toolbox.javabuilder.MWArray;
+import com.mathworks.toolbox.javabuilder.MWClassID;
+import com.mathworks.toolbox.javabuilder.MWComplexity;
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
+
+import fudan.se.pageranking.PageRanking;
+
+/**
+ * @author linyun
+ *
+ */
+public class PageRankingTest {
+
+	@Test
+	public void testPageRanking() {
+		
+		MWNumericArray iMatrix = null;
+		MWNumericArray jMatrix = null;
+		MWNumericArray nMatrix = null;
+		MWNumericArray x0Matrix = null;
+		Object[] inputs = null;
+		Object[] results = null;
+		try {
+			
+			int[] i = {1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8};
+			int[] j = {3, 1, 4, 1, 1, 2, 3, 2, 6, 7, 8, 4, 5, 8, 6};
+			int[] n = {8};
+			int[] x0 = {1, 1, 1, 1, 1, 1, 1, 1};
+			
+			/**
+			 * The first parameter int[] specify the dimensions of a matrix.
+			 */
+			iMatrix = MWNumericArray.newInstance(new int[]{1, i.length}, MWClassID.DOUBLE, MWComplexity.REAL);
+			jMatrix = MWNumericArray.newInstance(new int[]{1, j.length}, MWClassID.DOUBLE, MWComplexity.REAL);
+			
+			for(int k=1; k<=i.length; k++){
+				iMatrix.set(new int[]{1, k}, i[k-1]);
+				jMatrix.set(new int[]{1, k}, j[k-1]);
+			}
+			
+			nMatrix = MWNumericArray.newInstance(new int[]{1, 1}, MWClassID.DOUBLE, MWComplexity.REAL);
+			nMatrix.set(new int[]{1, 1}, 8);
+			
+			x0Matrix = MWNumericArray.newInstance(new int[]{x0.length, 1}, MWClassID.DOUBLE, MWComplexity.REAL);
+			for(int k=1; k<=x0.length; k++){
+				x0Matrix.set(new int[]{k, 1}, 0.125);
+			}
+			
+			PageRanking pageRanking = new PageRanking();
+			
+			inputs = new Object[4];
+			inputs[0] = iMatrix;
+			inputs[1] = jMatrix;
+			inputs[2] = nMatrix;
+			inputs[3] = x0Matrix;
+			
+			results = pageRanking.pageRanking(1, inputs);
+			
+			//pageRanking.pageRanking(inputs, results);
+			System.out.println(results[0]);
+			
+			MWNumericArray resultMatrix = (MWNumericArray)results[0];
+			Double value = (Double)resultMatrix.get(new int[]{1, 1});
+			
+			assertEquals(value, 0.0304, 0.0001);
+			
+		} catch (MWException e) {
+			e.printStackTrace();
+		} finally {
+			MWArray.disposeArray(iMatrix);
+			MWArray.disposeArray(jMatrix);
+			MWArray.disposeArray(nMatrix);
+			MWArray.disposeArray(x0Matrix);
+			MWArray.disposeArray(inputs);
+			MWArray.disposeArray(results);
+		}
+	}
+
+}
