484fab00aa6f9bfde1d527e8c42719009963a60f
linyun <llmhyy@gmail.com>
2014-01-24 20:26:15 +0800

    Change the column width and make the similarity values greater than threshold yellow

--- reflexactoring.diagram/src/reflexactoring/diagram/view/ModuleUnitsSimilarityView.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/view/ModuleUnitsSimilarityView.java
@@ -4,15 +4,20 @@ import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Table;
@@ -30,32 +35,33 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 
 	private TableViewer tableViewer;
 	private Composite composite;
-	
+
 	public ModuleUnitsSimilarityView() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		
+
 		this.composite = parent;
-		
-		if(Settings.similarityTable != null && Settings.similarityTable.size() > 0){
+
+		if (Settings.similarityTable != null
+				&& Settings.similarityTable.size() > 0) {
 			refreshUI(Settings.similarityTable);
 		}
 	}
-	
-	public void refreshUI(ModuleUnitsSimilarityTable similarityTable){
-		
-		for(Control control: this.composite.getChildren()){
+
+	public void refreshUI(ModuleUnitsSimilarityTable similarityTable) {
+
+		for (Control control : this.composite.getChildren()) {
 			control.dispose();
 		}
-		
+
 		tableViewer = new TableViewer(this.composite, SWT.MULTI | SWT.H_SCROLL
 				| SWT.FULL_SELECTION | SWT.BORDER);
-		
+
 		createColumns(this.composite, tableViewer);
-		
+
 		final Table table = tableViewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
@@ -70,23 +76,24 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 		gridData.grabExcessVerticalSpace = true;
 		gridData.horizontalAlignment = GridData.FILL;
 		tableViewer.getControl().setLayoutData(gridData);
-		
+
 		this.composite.redraw();
 		this.composite.layout();
 	}
-	
+
 	private void createColumns(final Composite parent, final TableViewer viewer) {
-		
-		String[] titles = new String[Settings.similarityTable.getColumnNumber()+1];
-		int[] bounds = new int[Settings.similarityTable.getColumnNumber()+1];
+
+		String[] titles = new String[Settings.similarityTable.getColumnNumber() + 1];
+		int[] bounds = new int[Settings.similarityTable.getColumnNumber() + 1];
 		titles[0] = "Module";
 		bounds[0] = 100;
-		ModuleUnitsSimilarity moduleSimilarity = Settings.similarityTable.get(0);
-		for(int i=1; i<titles.length; i++){
-			titles[i] = moduleSimilarity.getUnits().get(i-1).getSimpleName();
-			bounds[i] = 200;
+		ModuleUnitsSimilarity moduleSimilarity = Settings.similarityTable
+				.get(0);
+		for (int i = 1; i < titles.length; i++) {
+			titles[i] = moduleSimilarity.getUnits().get(i - 1).getSimpleName();
+			bounds[i] = 150;
 		}
-		
+
 		/**
 		 * create column for describing module.
 		 */
@@ -94,7 +101,7 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
-				ModuleUnitsSimilarity moduleSimilarity = (ModuleUnitsSimilarity)element;
+				ModuleUnitsSimilarity moduleSimilarity = (ModuleUnitsSimilarity) element;
 				return moduleSimilarity.getModule().getName();
 			}
 		});
@@ -102,19 +109,28 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 		/**
 		 * create column for describing types.
 		 */
-		for(int i=1; i<titles.length; i++){
+		for (int i = 1; i < titles.length; i++) {
 			final int index = i;
 			col = createTableViewerColumn(titles[i], bounds[i], i);
 			col.setLabelProvider(new ColumnLabelProvider() {
 				@Override
 				public String getText(Object element) {
-					ModuleUnitsSimilarity moduleSimilarity = (ModuleUnitsSimilarity)element;
-					return String.valueOf(moduleSimilarity.getValues()[index-1]);
+					ModuleUnitsSimilarity moduleSimilarity = (ModuleUnitsSimilarity) element;
+					return String.valueOf(moduleSimilarity.getValues()[index - 1]);
+				}
+				
+				@Override
+				public void update(ViewerCell cell) {
+					super.update(cell);
+					if(Double.valueOf(cell.getText()) >= Double.valueOf(ReflexactoringUtil.getMappingThreshold())){
+						cell.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW));
+					}
+					
 				}
 			});
 		}
 	}
-	
+
 	private TableViewerColumn createTableViewerColumn(String title, int bound,
 			final int colNumber) {
 		final TableViewerColumn viewerColumn = new TableViewerColumn(
@@ -124,9 +140,10 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 		column.setWidth(bound);
 		column.setResizable(true);
 		column.setMoveable(true);
-		
-		viewerColumn.setEditingSupport(new SimilarityTableEditingSupport(tableViewer, colNumber));
-		
+
+		viewerColumn.setEditingSupport(new SimilarityTableEditingSupport(
+				tableViewer, colNumber));
+
 		return viewerColumn;
 	}
 
@@ -146,21 +163,25 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 	public Composite getComposite() {
 		return composite;
 	}
-	
-	public class SimilarityTableEditingSupport extends EditingSupport{
+
+	public class SimilarityTableEditingSupport extends EditingSupport {
 		private CellEditor editor;
 		private TableViewer viewer;
 		private int columnNo;
-		
-		public SimilarityTableEditingSupport(TableViewer viewer, int columnNo){
+
+		public SimilarityTableEditingSupport(TableViewer viewer, int columnNo) {
 			super(viewer);
 			this.columnNo = columnNo;
 			this.viewer = viewer;
 			this.editor = new TextCellEditor(viewer.getTable());
 		}
 
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.viewers.EditingSupport#getCellEditor(java.lang.Object)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.eclipse.jface.viewers.EditingSupport#getCellEditor(java.lang.
+		 * Object)
 		 */
 		@Override
 		protected CellEditor getCellEditor(Object element) {
@@ -168,7 +189,8 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 		}
 
 		/**
-		 * if column number is 0, it stands for the name of module which cannot be edited.
+		 * if column number is 0, it stands for the name of module which cannot
+		 * be edited.
 		 */
 		@Override
 		protected boolean canEdit(Object element) {
@@ -177,38 +199,53 @@ public class ModuleUnitsSimilarityView extends ViewPart {
 
 		@Override
 		protected Object getValue(Object element) {
-			if(element instanceof ModuleUnitsSimilarity){
-				ModuleUnitsSimilarity moduleUnitsSimilarity = (ModuleUnitsSimilarity)element;
-				
-				if(0 == columnNo){
+			if (element instanceof ModuleUnitsSimilarity) {
+				ModuleUnitsSimilarity moduleUnitsSimilarity = (ModuleUnitsSimilarity) element;
+
+				if (0 == columnNo) {
 					return moduleUnitsSimilarity.getModule().getName();
-				}
-				else{
-					return String.valueOf(moduleUnitsSimilarity.getValues()[columnNo-1]);
+				} else {
+					return String
+							.valueOf(moduleUnitsSimilarity.getValues()[columnNo - 1]);
 				}
 			}
-			
+
 			return null;
 		}
-		
+
 		@Override
 		protected void setValue(Object element, Object value) {
-			if(element instanceof ModuleUnitsSimilarity){
-				ModuleUnitsSimilarity moduleUnitsSimilarity = (ModuleUnitsSimilarity)element;
-				
-				if(columnNo > 0){
+			if (element instanceof ModuleUnitsSimilarity) {
+				ModuleUnitsSimilarity moduleUnitsSimilarity = (ModuleUnitsSimilarity) element;
+
+				if (columnNo > 0) {
 					/**
 					 * need a check for string value.
 					 */
-					String numberStr = (String)value;
-					if(ReflexactoringUtil.checkNumber(numberStr)){
+					String numberStr = (String) value;
+					if (ReflexactoringUtil.checkNumber(numberStr)) {
 						double similarity = Double.valueOf(numberStr);
-						moduleUnitsSimilarity.getValues()[columnNo-1] = similarity;
-						viewer.update(element, null);						
+						moduleUnitsSimilarity.getValues()[columnNo - 1] = similarity;
+						viewer.update(element, null);
 					}
 					viewer.refresh();
 				}
 			}
+		}
+
+	}
+
+	public class ValueLabelLayout extends ColumnLabelProvider {
+		@Override
+		public void update(ViewerCell cell) {
+			if(Double.valueOf(cell.getText()) >= Double.valueOf(ReflexactoringUtil.getMappingThreshold())){
+				StyleRange myStyledRange = new StyleRange(16, 2, null, Display
+						.getCurrent().getSystemColor(SWT.COLOR_YELLOW));
+				StyleRange[] range = { myStyledRange };
+				cell.setStyleRanges(range);
+				super.update(cell);
+			}
+			
 		}
 		
 		
