8f3e373777ce92baf7dd0ef89a8a0485d939e619
linyun <llmhyy@gmail.com>
2014-02-07 14:38:20 +0800

    Finish another strategy to generate population by *making sure that all the genotype conforms to hard constraints*

--- reflexactoring.diagram/src/reflexactoring/diagram/action/recommend/suboptimal/GeneticOptimizer.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/recommend/suboptimal/GeneticOptimizer.java
@@ -4,6 +4,8 @@
 package reflexactoring.diagram.action.recommend.suboptimal;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
@@ -26,7 +28,109 @@ public class GeneticOptimizer {
 	
 	private Matrix weightVector;
 	private Matrix x0Vector;
+	
+	/**
+	 * The below two variables stand for the map relation between a solution and its corresponding module-unit pair.
+	 * A solution actually specify how a unit/member will be mapped to a module. For example, a solution will be
+	 * like [0, 1, 0, 1, 0], which means that there are five possible maps between some modules an some units. More
+	 * specific, there are two modules A, B and three units a, b and c. The five possible maps are A->a, A->b, B->a,
+	 * B->b, B->c. And the above vector stands for A->b and B->b are chosen while others are not. And now, the below
+	 * two variables stands for which maps are a component in the vector stands for.
+	 * 
+	 * The mapping relation is between the index of a component and a pair of index (one is for index of module; one is
+	 * for the index of unit). By this means, I am able to correspond the relation.
+	 */
 	private ArrayList<int[]> relationMap;
+	private HashMap<ModuleUnitCorrespondence, Integer> reverseRelationMap;
+	
+	public class ModuleUnitCorrespondence{
+		private int moduleIndex;
+		private int unitIndex;
+		
+		
+		/**
+		 * @param moduleIndex
+		 * @param unitIndex
+		 */
+		public ModuleUnitCorrespondence(int moduleIndex, int unitIndex) {
+			super();
+			this.moduleIndex = moduleIndex;
+			this.unitIndex = unitIndex;
+		}
+		
+		
+		
+		/* (non-Javadoc)
+		 * @see java.lang.Object#hashCode()
+		 */
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + moduleIndex;
+			result = prime * result + unitIndex;
+			return result;
+		}
+
+
+
+		/* (non-Javadoc)
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ModuleUnitCorrespondence other = (ModuleUnitCorrespondence) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (moduleIndex != other.moduleIndex)
+				return false;
+			if (unitIndex != other.unitIndex)
+				return false;
+			return true;
+		}
+
+
+
+		/**
+		 * @return the moduleIndex
+		 */
+		public int getModuleIndex() {
+			return moduleIndex;
+		}
+		/**
+		 * @param moduleIndex the moduleIndex to set
+		 */
+		public void setModuleIndex(int moduleIndex) {
+			this.moduleIndex = moduleIndex;
+		}
+		/**
+		 * @return the unitIndex
+		 */
+		public int getUnitIndex() {
+			return unitIndex;
+		}
+		/**
+		 * @param unitIndex the unitIndex to set
+		 */
+		public void setUnitIndex(int unitIndex) {
+			this.unitIndex = unitIndex;
+		}
+
+
+
+		private GeneticOptimizer getOuterType() {
+			return GeneticOptimizer.this;
+		}
+		
+		
+	}
 	
 	public ArrayList<int[]> getRelationMap(){
 		return this.relationMap;
@@ -61,7 +165,7 @@ public class GeneticOptimizer {
 		
 		FitnessComputingFactor computingFactor = buildComputingFactor(similarityTable, modules, units);
 		
-		Genotype gene = computeOptimalResult(computingFactor);
+		Genotype gene = computeOptimalResult(computingFactor, modules, units, similarityTable);
 		
 		return gene;
 	}
@@ -69,18 +173,23 @@ public class GeneticOptimizer {
 	public Genotype optimize(UnitMemberWrapperList members, ArrayList<ModuleWrapper> modules){
 		double[][] similarityTable = new double[modules.size()][members.size()];
 		
+		for(int i=0; i<modules.size(); i++){
+			for(int j=0; j<members.size(); j++){
+				similarityTable[i][j] = 1;
+			}
+		}
+		
 		FitnessComputingFactor computingFactor = buildComputingFactor(similarityTable, modules, members);
 		
-		Genotype gene = computeOptimalResult(computingFactor);
+		Genotype gene = computeOptimalResult(computingFactor, modules, members, similarityTable);
 		
 		return gene;
 	}
 	
-	@SuppressWarnings("unchecked")
 	private FitnessComputingFactor buildComputingFactor(double[][] similarityTable, ArrayList<ModuleWrapper> modules, 
 			ArrayList<? extends LowLevelGraphNode> lowLevelUnits){
 		/**
-		 * In the method as extract*LevelRelation, the weight vector and x0 vector will be initialized as well.
+		 * In the method as extractRelation, the weight vector and x0 vector will be initialized as well.
 		 */
 		Matrix relationMatrix = extractRelation(similarityTable, modules, lowLevelUnits);
 		
@@ -102,9 +211,10 @@ public class GeneticOptimizer {
 	/**
 	 * 
 	 */
-	private Genotype computeOptimalResult(FitnessComputingFactor computingFactor) {
+	private Genotype computeOptimalResult(FitnessComputingFactor computingFactor,
+			ArrayList<ModuleWrapper> modules, ArrayList<? extends LowLevelGraphNode> lowLevelNodes, double[][] similarityTable) {
 		
-		Population population = generatePopulation(computingFactor);
+		Population population = generatePopulation(computingFactor, modules, lowLevelNodes, similarityTable);
 		System.out.println(population.getOptimalGene().getFitness());
 		
 		for(int i=0; i<Integer.valueOf(ReflexactoringUtil.getIterationNumber()); i++){
@@ -115,23 +225,17 @@ public class GeneticOptimizer {
 		return population.getOptimalGene();
 	}
 	
-	private Population generatePopulation(FitnessComputingFactor computingFactor/*, ArrayList<ModuleWrapper> modules, ArrayList<? extends GraphNode> lowLevelNodes*/){
-		
-		//assert(lowLevelNodes != null && lowLevelNodes.size() > 0);
+	private Population generatePopulation(FitnessComputingFactor computingFactor,
+			ArrayList<ModuleWrapper> modules, ArrayList<? extends LowLevelGraphNode> lowLevelNodes, double[][] similarityTable){
 		
 		int dimension = computingFactor.getX0Vector().getRowDimension();
 		
 		Population population = new Population();
 		for(int i=0; i<Integer.valueOf(ReflexactoringUtil.getPopulationSize()); i++){
-			int[] seed = new int[dimension];
-			for(int j=0; j<dimension; j++){
-				if(Math.random()>=0.5){
-					seed[j] = 1;
-				}
-				else{
-					seed[j] = 0;
-				}
-			}
+			
+			//int[] seed = randomSeed(dimension);
+			
+			int[] seed = randomSeedConformedToHardConstraints(dimension, lowLevelNodes, modules, similarityTable);
 			
 			Genotype gene = new Genotype(seed);
 			gene.setFitness(gene.computeFitness(computingFactor));
@@ -142,6 +246,94 @@ public class GeneticOptimizer {
 		population.updateOptimalGene();
 		
 		return population;
+	}
+	
+	private int[] randomSeedConformedToHardConstraints(int dimension, ArrayList<? extends LowLevelGraphNode> lowLevelNodes, 
+			ArrayList<ModuleWrapper> modules, double[][] similarityTable){
+		int[] seed = new int[dimension];
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<LowLevelGraphNode>[] buckets = new ArrayList[modules.size()];
+		for(int i=0; i<buckets.length; i++){
+			buckets[i] = new ArrayList<>();
+		}
+		
+		do{
+			seed = new int[dimension];
+			for(int i=0; i<buckets.length; i++){
+				buckets[i].clear();
+			}
+			
+			
+			for(int q=0; q<lowLevelNodes.size(); q++){
+				ArrayList<Integer> validModuleIndex = new ArrayList<>();
+				
+				/**
+				 * find all the valid modules to assign units
+				 */
+				for(int p=0; p<modules.size(); p++){
+					if(similarityTable[p][q] > Double.valueOf(ReflexactoringUtil.getMappingThreshold())){
+						validModuleIndex.add(p);
+					}
+				}
+				assert(validModuleIndex.size() > 0);
+				
+				/**
+				 * In all the buckets (modules), if there are some buckets which have not been assigned
+				 * any units (low level nodes), the algorithm will favor them some priority to be assigned
+				 * first.
+				 */
+				if(isSomeBucketEmpty(buckets)){
+					Iterator<Integer> iterator = validModuleIndex.iterator();
+					while(iterator.hasNext()){
+						Integer moduleIndex = iterator.next();
+						if(buckets[moduleIndex].size() != 0){
+							iterator.remove();
+						}
+					}
+				}
+				assert(validModuleIndex.size() > 0);
+				
+				double random = Math.random();
+				
+				int randomIndex = (int)(random*validModuleIndex.size());
+				int chosenModule = validModuleIndex.get(randomIndex);
+				int chosenUnit = q;
+				
+				ModuleUnitCorrespondence chosenPair = new ModuleUnitCorrespondence(chosenModule, chosenUnit);
+				buckets[chosenModule].add(lowLevelNodes.get(chosenUnit));
+				
+				Integer componentIndex = this.reverseRelationMap.get(chosenPair);
+				seed[componentIndex] = 1;
+			}
+		}while(isSomeBucketEmpty(buckets));
+		
+		
+		return seed;
+	}
+	
+	private boolean isSomeBucketEmpty(ArrayList<? extends LowLevelGraphNode>[] buckets){
+		for(ArrayList<? extends LowLevelGraphNode> bucket: buckets){
+			if(bucket.size() == 0){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private int[] randomSeed(int dimension){
+		int[] seed = new int[dimension];
+		for(int j=0; j<dimension; j++){
+			if(Math.random()>=0.5){
+				seed[j] = 1;
+			}
+			else{
+				seed[j] = 0;
+			}
+		}
+		
+		return seed;
 	}
 	
 	/**
@@ -308,12 +500,17 @@ public class GeneticOptimizer {
 		ArrayList<Double> weightVectorList = new ArrayList<>();
 		ArrayList<Integer> x0VectorList = new ArrayList<>();
 		ArrayList<int[]> relationMap = new ArrayList<>();
+		HashMap<ModuleUnitCorrespondence, Integer> reverseRelationMap = new HashMap<>();
 		
+		Integer mapIndex = 0;
 		for(int i=0; i<highLevelNumber; i++){
 			for(int j=0; j<lowLevelNumber; j++){
 				if(similarityTable[i][j] >= Double.valueOf(ReflexactoringUtil.getMappingThreshold())){
 					relationMatrix.set(i, j, 1);
-					relationMap.add(new int[]{i, j});
+					
+					int[] moduleUnitPair = new int[]{i, j};
+					relationMap.add(moduleUnitPair);
+					reverseRelationMap.put(new ModuleUnitCorrespondence(i, j), mapIndex++);
 					/**
 					 * initial weight
 					 */
@@ -339,6 +536,7 @@ public class GeneticOptimizer {
 		this.weightVector = GeneticUtil.convertRowVectorToMatrix(weightVectorList);
 		this.x0Vector = GeneticUtil.convertColumnVectorToMatrx(x0VectorList);
 		this.relationMap = relationMap;
+		this.reverseRelationMap = reverseRelationMap;
 		
 		return relationMatrix;
 	}
