6bd0a41d1b478f60725ec3e289e701a55ef2570d
linyun <llmhyy@gmail.com>
2014-03-05 16:19:33 +0800

    Change a selector to improve the diversity of GA.

--- reflexactoring.diagram/src/reflexactoring/diagram/action/recommend/suboptimal/AbstractAsexualCrossoverer.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/recommend/suboptimal/AbstractAsexualCrossoverer.java
@@ -3,7 +3,9 @@
  */
 package reflexactoring.diagram.action.recommend.suboptimal;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * @author linyun
@@ -40,18 +42,51 @@ public abstract class AbstractAsexualCrossoverer implements Crossoverer {
 		crosssoverPopulation.addAll(selectedPopulation);
 		Collections.sort(crosssoverPopulation, new GeneComparator());
 		Population newCrossoverPopulation = new Population();
+				
+		int len = crosssoverPopulation.size()/4;
+		for (int i = 0; i < 2*len; i++) {
+			newCrossoverPopulation.add(crosssoverPopulation.get(i));
+		}
+		
+		ArrayList<Genotype> candidateDistinctList = getDistinctCandidateList(crosssoverPopulation);
+		int cursor = 0;
+		
+		HashMap<Genotype, Integer> occuringFreqMap = new HashMap<>();
+		
+		for(int i=0; i<newCrossoverPopulation.size(); i++){
+			Genotype genotype = newCrossoverPopulation.get(i);
+			Integer freq = occuringFreqMap.get(genotype);
+			if(freq == null){
+				occuringFreqMap.put(genotype, 1);
+			}
+			else{
+				if(freq == 1){
+					occuringFreqMap.put(genotype, 2);
+				}
+				else if(freq > 1){
+					if(cursor < candidateDistinctList.size()){
+						newCrossoverPopulation.set(i, candidateDistinctList.get(cursor++));
+					}
+				}
+			}
+			
+			if(cursor >= candidateDistinctList.size()){
+				break;
+			}
+		}
+		Collections.sort(newCrossoverPopulation, new GeneComparator());
 		
 		/*for (int i = 0; i < crosssoverPopulation.size()/2; i++) {
 			newCrossoverPopulation.add(crosssoverPopulation.get(i));
 		}*/
 		
-		int len = crosssoverPopulation.size()/4;
-		for (int i = 0; i < len; i++) {
+		/*int len = crosssoverPopulation.size()/4;
+		for (int i = 0; i < 2*len; i++) {
 			newCrossoverPopulation.add(crosssoverPopulation.get(i));
-		}
-		for (int i = 2*len; i < 3*len; i++) {
+		}*/
+		/*for (int i = 2*len; i < 3*len; i++) {
 			newCrossoverPopulation.add(crosssoverPopulation.get(i));
-		}
+		}*/
 
 		//newCrossoverPopulation.setOptimalGene(selectedPopulation.getOptimalGene());
 		//newCrossoverPopulation.updateOptimalGene();
@@ -63,4 +98,17 @@ public abstract class AbstractAsexualCrossoverer implements Crossoverer {
 		return crosssoverPopulation;*/
 	}
 
+	private ArrayList<Genotype> getDistinctCandidateList(Population crosssoverPopulation){
+		ArrayList<Genotype> distinctList = new ArrayList<>();
+		
+		int len = crosssoverPopulation.size()/4;
+		for(int i=2*len; i<crosssoverPopulation.size(); i++){
+			Genotype gene = crosssoverPopulation.get(i);
+			if(!distinctList.contains(gene)){
+				distinctList.add(gene);
+			}
+		}
+		
+		return distinctList;
+	}
 }
