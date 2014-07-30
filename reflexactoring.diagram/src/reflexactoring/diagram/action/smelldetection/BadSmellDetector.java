7b2f213a35ec279b213e66fc13aa03b7f1cb1f11
linyun <llmhyy@gmail.com>
2014-07-30 19:32:17 +0800

    Refactor the BadSmellDetector

--- reflexactoring.diagram/src/reflexactoring/diagram/action/smelldetection/BadSmellDetector.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/smelldetection/BadSmellDetector.java
@@ -4,45 +4,14 @@
 package reflexactoring.diagram.action.smelldetection;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eposoft.jccd.data.ASourceUnit;
-import org.eposoft.jccd.data.JCCDFile;
-import org.eposoft.jccd.data.SimilarityGroup;
-import org.eposoft.jccd.data.SimilarityGroupManager;
-import org.eposoft.jccd.data.SourceUnitPosition;
-import org.eposoft.jccd.data.ast.ANode;
-import org.eposoft.jccd.data.ast.NodeTypes;
-import org.eposoft.jccd.detectors.APipeline;
-import org.eposoft.jccd.detectors.ASTDetector;
-import org.eposoft.jccd.preprocessors.java.GeneralizeArrayInitializers;
-import org.eposoft.jccd.preprocessors.java.GeneralizeClassDeclarationNames;
-import org.eposoft.jccd.preprocessors.java.GeneralizeMethodArgumentTypes;
-import org.eposoft.jccd.preprocessors.java.GeneralizeMethodCallNames;
-import org.eposoft.jccd.preprocessors.java.GeneralizeMethodReturnTypes;
-import org.eposoft.jccd.preprocessors.java.GeneralizeVariableDeclarationTypes;
-import org.eposoft.jccd.preprocessors.java.GeneralizeVariableNames;
-import org.eposoft.jccd.preprocessors.java.RemoveAnnotations;
-import org.eposoft.jccd.preprocessors.java.RemoveAssertions;
-import org.eposoft.jccd.preprocessors.java.RemoveEmptyBlocks;
-import org.eposoft.jccd.preprocessors.java.RemoveSemicolons;
-
-import reflexactoring.diagram.action.smelldetection.bean.CloneInstance;
-import reflexactoring.diagram.action.smelldetection.bean.CloneSet;
 import reflexactoring.diagram.action.smelldetection.refactoringopportunities.ExtractUtilityClassOpportunity;
 import reflexactoring.diagram.action.smelldetection.refactoringopportunities.MoveMethodOpportunity;
-import reflexactoring.diagram.action.smelldetection.refactoringopportunities.PullUpMemberToInterfaceOpportunity;
 import reflexactoring.diagram.action.smelldetection.refactoringopportunities.RefactoringOpportunity;
 import reflexactoring.diagram.action.smelldetection.refactoringopportunities.precondition.PullUpMemberPrecondition;
 import reflexactoring.diagram.action.smelldetection.refactoringopportunities.precondition.RefactoringPrecondition;
-import reflexactoring.diagram.bean.ICompilationUnitWrapper;
 import reflexactoring.diagram.bean.ModuleWrapper;
 import reflexactoring.diagram.bean.ProgramModel;
-import reflexactoring.diagram.bean.ProgramReference;
-import reflexactoring.diagram.bean.UnitMemberWrapper;
 import reflexactoring.diagram.util.ReflexactoringUtil;
 import reflexactoring.diagram.util.Settings;
 
@@ -51,149 +20,27 @@ import reflexactoring.diagram.util.Settings;
  *
  */
 public class BadSmellDetector {
-	ArrayList<RefactoringOpportunity> opporuntities = new ArrayList<>();
 	
-	public void detect(ProgramModel model){
-		ArrayList<CloneSet> cloneSets = detectClone(model);
-		model.setCloneSets(cloneSets);
-		
+	private ArrayList<RefactoringPrecondition> preconditionList = new ArrayList<>();
+	
+	public BadSmellDetector(){
 		ArrayList<ModuleWrapper> moduleList = ReflexactoringUtil.getModuleList(Settings.diagramPath);
-		
-		
-		ArrayList<RefactoringPrecondition> preconditionList = new ArrayList<>();
 		//preconditionList.add(new PullUpMemberToInterfaceOpportunity(null,null).new Precondition(moduleList));
-		//preconditionList.add(new PullUpMemberPrecondition(moduleList));	
-		//preconditionList.add(new ExtractUtilityClassOpportunity(null,null).new Precondition(moduleList));
+		preconditionList.add(new PullUpMemberPrecondition(moduleList));	
+		preconditionList.add(new ExtractUtilityClassOpportunity(null,null).new Precondition(moduleList));
 		preconditionList.add(new MoveMethodOpportunity(null, null).new Precodition());
+	}
+	
+	public ArrayList<RefactoringOpportunity> detect(ProgramModel model){
+		ArrayList<RefactoringOpportunity> opporuntities = new ArrayList<>();
+		
+		model.detectClone(model);
 		
 		for(RefactoringPrecondition precondition: preconditionList){
 			ArrayList<RefactoringOpportunity> oppList = precondition.detectOpportunities(model);
-			this.opporuntities.addAll(oppList);
+			opporuntities.addAll(oppList);
 		}
 		
-		System.currentTimeMillis();
-	}
-
-	private ArrayList<CloneSet> detectClone(ProgramModel model){
-		
-		ArrayList<JCCDFile> fileList = new ArrayList<JCCDFile>();
-		HashMap<String, ICompilationUnitWrapper> map = new HashMap<>();
-		
-		for(ICompilationUnitWrapper unit: model.getScopeCompilationUnitList()){
-			IResource resource = unit.getCompilationUnit().getResource();			
-			fileList.add(new JCCDFile(resource.getRawLocation().toFile()));
-			
-			String path = resource.getRawLocation().toFile().getAbsolutePath();
-			map.put(path, unit);
-		}
-		
-		APipeline<?> detector = new ASTDetector();
-		JCCDFile[] files = fileList.toArray(new JCCDFile[0]);
-		detector.setSourceFiles(files);
-		
-		detector.addOperator(new RemoveAnnotations());
-		//detector.addOperator(new RemoveSimpleMethods());
-		detector.addOperator(new RemoveSemicolons());
-		detector.addOperator(new RemoveAssertions());
-		detector.addOperator(new RemoveEmptyBlocks());		
-		detector.addOperator(new GeneralizeArrayInitializers());
-		detector.addOperator(new GeneralizeClassDeclarationNames());
-		detector.addOperator(new GeneralizeMethodArgumentTypes());
-		detector.addOperator(new GeneralizeMethodReturnTypes());
-		detector.addOperator(new GeneralizeVariableDeclarationTypes());
-		detector.addOperator(new GeneralizeMethodCallNames());
-		detector.addOperator(new GeneralizeVariableDeclarationTypes());
-		detector.addOperator(new GeneralizeVariableNames());
-		
-		SimilarityGroupManager manager = detector.process();
-		SimilarityGroup[] simGroups = manager.getSimilarityGroups();
-		
-		ArrayList<CloneSet> cloneSets = convertToCloneSets(simGroups, map);
-		
-		return cloneSets;
-	}
-
-	/**
-	 * In this method, clone sets will be constructed, in which every clone instance will contain not only file name, line numbers,
-	 * but its related unit member and program reference as well.
-	 * 
-	 * @param simGroups
-	 * @return
-	 */
-	private ArrayList<CloneSet> convertToCloneSets(SimilarityGroup[] simGroups, HashMap<String, ICompilationUnitWrapper> map) {
-		
-		ArrayList<CloneSet> sets = new ArrayList<>();
-		
-		for (int i = 0; i < simGroups.length; i++) {
-			final ASourceUnit[] nodes = simGroups[i].getNodes();
-			
-			CloneSet set = new CloneSet(String.valueOf(simGroups[i].getGroupId()));
-			
-			for (int j = 0; j < nodes.length; j++) {
-				
-				final SourceUnitPosition minPos = APipeline.getFirstNodePosition((ANode) nodes[j]);
-				final SourceUnitPosition maxPos = APipeline.getLastNodePosition((ANode) nodes[j]);
-
-				ANode fileNode = (ANode) nodes[j];
-				while (fileNode.getType() != NodeTypes.FILE.getType()) {
-					fileNode = fileNode.getParent();
-				}
-				
-				CloneInstance cloneInstance = new CloneInstance(set, fileNode.getText(), 
-						minPos.getLine(), maxPos.getLine());
-				
-				//set.getInstances().add(cloneInstance);
-				
-				UnitMemberWrapper member = findResidingUnitMember(cloneInstance, map);
-				if(member != null){
-					set.getInstances().add(cloneInstance);					
-				}
-			}
-			
-			if(set.getInstances().size() >= 2){
-				sets.add(set);				
-			}
-		}
-		
-		return sets;
-	}
-
-	/**
-	 * @param cloneInstance
-	 * @param map
-	 * @return
-	 */
-	private UnitMemberWrapper findResidingUnitMember(
-			CloneInstance cloneInstance,
-			HashMap<String, ICompilationUnitWrapper> map) {
-		ICompilationUnitWrapper unitWrapper = map.get(cloneInstance.getFileName());
-		CompilationUnit unit = unitWrapper.getJavaUnit();
-		for(UnitMemberWrapper member: unitWrapper.getMembers()){
-			ASTNode declaringNode = member.getJavaElement();
-			int startLine = unit.getLineNumber(declaringNode.getStartPosition());
-			int endLine = unit.getLineNumber(declaringNode.getStartPosition()+declaringNode.getLength());
-			
-			/**
-			 * Judge whether the cloned code fragment is located in the right scope.
-			 */
-			if(cloneInstance.getStartLineNumber() >= startLine && cloneInstance.getEndLineNumber() <= endLine){
-				cloneInstance.setMember(member);
-				
-				/**
-				 * Checking the program reference covered by this cloned code fragment.
-				 */
-				for(ProgramReference reference: member.getRefereePointList()){
-					int lineNumber = unit.getLineNumber(reference.getASTNode().getStartPosition());
-					if(lineNumber >= cloneInstance.getStartLineNumber() && lineNumber <= cloneInstance.getEndLineNumber()){
-						cloneInstance.getCoveringReferenceList().add(reference);
-					}
-				}
-				
-				return member;
-			}
-		}
-		
-		
-		return null;
+		return opporuntities;
 	}
 }
--- reflexactoring.diagram/src/reflexactoring/diagram/bean/ProgramModel.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/bean/ProgramModel.java
@@ -4,9 +4,33 @@
 package reflexactoring.diagram.bean;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eposoft.jccd.data.ASourceUnit;
+import org.eposoft.jccd.data.JCCDFile;
+import org.eposoft.jccd.data.SimilarityGroup;
+import org.eposoft.jccd.data.SimilarityGroupManager;
+import org.eposoft.jccd.data.SourceUnitPosition;
+import org.eposoft.jccd.data.ast.ANode;
+import org.eposoft.jccd.data.ast.NodeTypes;
+import org.eposoft.jccd.detectors.APipeline;
+import org.eposoft.jccd.detectors.ASTDetector;
+import org.eposoft.jccd.preprocessors.java.GeneralizeArrayInitializers;
+import org.eposoft.jccd.preprocessors.java.GeneralizeClassDeclarationNames;
+import org.eposoft.jccd.preprocessors.java.GeneralizeMethodArgumentTypes;
+import org.eposoft.jccd.preprocessors.java.GeneralizeMethodCallNames;
+import org.eposoft.jccd.preprocessors.java.GeneralizeMethodReturnTypes;
+import org.eposoft.jccd.preprocessors.java.GeneralizeVariableDeclarationTypes;
+import org.eposoft.jccd.preprocessors.java.GeneralizeVariableNames;
+import org.eposoft.jccd.preprocessors.java.RemoveAnnotations;
+import org.eposoft.jccd.preprocessors.java.RemoveAssertions;
+import org.eposoft.jccd.preprocessors.java.RemoveEmptyBlocks;
+import org.eposoft.jccd.preprocessors.java.RemoveSemicolons;
 
 import reflexactoring.Type;
 import reflexactoring.diagram.action.smelldetection.bean.CloneInstance;
@@ -20,7 +44,7 @@ public class ProgramModel{
 	private ArrayList<ICompilationUnitWrapper> scopeCompilationUnitList = new ArrayList<>();
 	private UnitMemberWrapperList scopeMemberList = new UnitMemberWrapperList();
 	private ArrayList<ProgramReference> referenceList = new ArrayList<>();
-	private ArrayList<CloneSet> cloneSets = new ArrayList<>();
+	private ArrayList<CloneSet> cloneSets;
 	
 	public ProgramModel clone(){
 		ProgramModel clonedModel = new ProgramModel();
@@ -564,5 +588,132 @@ public class ProgramModel{
 			}
 		}
 		return otherUnits;
+	}
+	
+	public void detectClone(ProgramModel model){
+		
+		if(this.cloneSets != null){
+			return;
+		}
+		
+		ArrayList<JCCDFile> fileList = new ArrayList<JCCDFile>();
+		HashMap<String, ICompilationUnitWrapper> map = new HashMap<>();
+		
+		for(ICompilationUnitWrapper unit: model.getScopeCompilationUnitList()){
+			IResource resource = unit.getCompilationUnit().getResource();			
+			fileList.add(new JCCDFile(resource.getRawLocation().toFile()));
+			
+			String path = resource.getRawLocation().toFile().getAbsolutePath();
+			map.put(path, unit);
+		}
+		
+		APipeline<?> detector = new ASTDetector();
+		JCCDFile[] files = fileList.toArray(new JCCDFile[0]);
+		detector.setSourceFiles(files);
+		
+		detector.addOperator(new RemoveAnnotations());
+		//detector.addOperator(new RemoveSimpleMethods());
+		detector.addOperator(new RemoveSemicolons());
+		detector.addOperator(new RemoveAssertions());
+		detector.addOperator(new RemoveEmptyBlocks());		
+		detector.addOperator(new GeneralizeArrayInitializers());
+		detector.addOperator(new GeneralizeClassDeclarationNames());
+		detector.addOperator(new GeneralizeMethodArgumentTypes());
+		detector.addOperator(new GeneralizeMethodReturnTypes());
+		detector.addOperator(new GeneralizeVariableDeclarationTypes());
+		detector.addOperator(new GeneralizeMethodCallNames());
+		detector.addOperator(new GeneralizeVariableDeclarationTypes());
+		detector.addOperator(new GeneralizeVariableNames());
+		
+		SimilarityGroupManager manager = detector.process();
+		SimilarityGroup[] simGroups = manager.getSimilarityGroups();
+		
+		ArrayList<CloneSet> cloneSets = convertToCloneSets(simGroups, map);
+		
+		this.cloneSets = cloneSets;
+	}
+
+	/**
+	 * In this method, clone sets will be constructed, in which every clone instance will contain not only file name, line numbers,
+	 * but its related unit member and program reference as well.
+	 * 
+	 * @param simGroups
+	 * @return
+	 */
+	private ArrayList<CloneSet> convertToCloneSets(SimilarityGroup[] simGroups, HashMap<String, ICompilationUnitWrapper> map) {
+		
+		ArrayList<CloneSet> sets = new ArrayList<>();
+		
+		for (int i = 0; i < simGroups.length; i++) {
+			final ASourceUnit[] nodes = simGroups[i].getNodes();
+			
+			CloneSet set = new CloneSet(String.valueOf(simGroups[i].getGroupId()));
+			
+			for (int j = 0; j < nodes.length; j++) {
+				
+				final SourceUnitPosition minPos = APipeline.getFirstNodePosition((ANode) nodes[j]);
+				final SourceUnitPosition maxPos = APipeline.getLastNodePosition((ANode) nodes[j]);
+
+				ANode fileNode = (ANode) nodes[j];
+				while (fileNode.getType() != NodeTypes.FILE.getType()) {
+					fileNode = fileNode.getParent();
+				}
+				
+				CloneInstance cloneInstance = new CloneInstance(set, fileNode.getText(), 
+						minPos.getLine(), maxPos.getLine());
+				
+				//set.getInstances().add(cloneInstance);
+				
+				UnitMemberWrapper member = findResidingUnitMember(cloneInstance, map);
+				if(member != null){
+					set.getInstances().add(cloneInstance);					
+				}
+			}
+			
+			if(set.getInstances().size() >= 2){
+				sets.add(set);				
+			}
+		}
+		
+		return sets;
+	}
+
+	/**
+	 * @param cloneInstance
+	 * @param map
+	 * @return
+	 */
+	private UnitMemberWrapper findResidingUnitMember(
+			CloneInstance cloneInstance,
+			HashMap<String, ICompilationUnitWrapper> map) {
+		ICompilationUnitWrapper unitWrapper = map.get(cloneInstance.getFileName());
+		CompilationUnit unit = unitWrapper.getJavaUnit();
+		for(UnitMemberWrapper member: unitWrapper.getMembers()){
+			ASTNode declaringNode = member.getJavaElement();
+			int startLine = unit.getLineNumber(declaringNode.getStartPosition());
+			int endLine = unit.getLineNumber(declaringNode.getStartPosition()+declaringNode.getLength());
+			
+			/**
+			 * Judge whether the cloned code fragment is located in the right scope.
+			 */
+			if(cloneInstance.getStartLineNumber() >= startLine && cloneInstance.getEndLineNumber() <= endLine){
+				cloneInstance.setMember(member);
+				
+				/**
+				 * Checking the program reference covered by this cloned code fragment.
+				 */
+				for(ProgramReference reference: member.getRefereePointList()){
+					int lineNumber = unit.getLineNumber(reference.getASTNode().getStartPosition());
+					if(lineNumber >= cloneInstance.getStartLineNumber() && lineNumber <= cloneInstance.getEndLineNumber()){
+						cloneInstance.getCoveringReferenceList().add(reference);
+					}
+				}
+				
+				return member;
+			}
+		}
+		
+		
+		return null;
 	}
 }
