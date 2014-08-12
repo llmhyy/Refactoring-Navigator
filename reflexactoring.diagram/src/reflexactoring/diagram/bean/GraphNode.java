ec667757ecf3afd79a9491762a6f576be87339ed
linyun <llmhyy@gmail.com>
2014-08-12 13:30:21 +0800

    Add some explanation by Javadoc

--- reflexactoring.diagram/src/reflexactoring/diagram/bean/GraphNode.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/bean/GraphNode.java
@@ -7,6 +7,11 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
+ * The data structure of module list is changed as well. For now, we can define the term *call* in a general way. 
+ * A call can be further distinguished in dependency, creation and etc. We support dependency and creation relationships 
+ * for now. Different calls are maintained in a attribute (hashmap<node, referencing_detail>) of a node, indicating a 
+ * node call the other node with some referencing detail. And a referencing detail is a hashmap<type, times> as well, 
+ * indicating how many times a certain relation occurs.
  * @author linyun
  *
  */
