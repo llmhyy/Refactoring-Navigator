879096f3053c228ca3ab6548d1af4f8c76e9c3ff
linyun <llmhyy@gmail.com>
2014-08-14 14:10:22 +0800

    Enhance the showing/hiding logic

--- reflexactoring.diagram/src/reflexactoring/diagram/action/popup/HideAllTypeDependencies.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/popup/HideAllTypeDependencies.java
@@ -1,14 +1,14 @@
 package reflexactoring.diagram.action.popup;
 
-import java.util.ArrayList;
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.gmf.runtime.common.ui.action.AbstractActionDelegate;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramRootEditPart;
 import org.eclipse.ui.IObjectActionDelegate;
 
-import reflexactoring.diagram.action.DiagramUpdater;
-import reflexactoring.diagram.bean.ModuleWrapper;
-import reflexactoring.diagram.util.ReflexactoringUtil;
+import reflexactoring.Reflexactoring;
+import reflexactoring.TypeDependency;
+import reflexactoring.diagram.edit.parts.TypeDependencyEditPart;
+import reflexactoring.diagram.util.GEFDiagramUtil;
 import reflexactoring.diagram.util.Settings;
 
 public class HideAllTypeDependencies extends AbstractActionDelegate implements IObjectActionDelegate{
@@ -19,8 +19,16 @@ public class HideAllTypeDependencies extends AbstractActionDelegate implements I
 	@Override
 	protected void doRun(IProgressMonitor progressMonitor) {
 		Settings.enableVisibility = false;
-		ArrayList<ModuleWrapper> moduleList = ReflexactoringUtil.getModuleList(Settings.diagramPath);
-		new DiagramUpdater().generateReflexionModel(moduleList, Settings.scope.getScopeCompilationUnitList());
+		
+		DiagramRootEditPart diagramRoot = GEFDiagramUtil.getRootEditPart();
+		
+		Reflexactoring reflexactoring = GEFDiagramUtil.findReflexactoring(diagramRoot);
+		for(TypeDependency dep: reflexactoring.getTypeDependencies()){
+			TypeDependencyEditPart part = GEFDiagramUtil.findCorrespondingTypeDependencyEditPart(diagramRoot, dep);
+			part.getFigure().setVisible(false);
+		}
+		//ArrayList<ModuleWrapper> moduleList = ReflexactoringUtil.getModuleList(Settings.diagramPath);
+		//new DiagramUpdater().generateReflexionModel(moduleList, Settings.scope.getScopeCompilationUnitList());
 	}
 
 }
--- reflexactoring.diagram/src/reflexactoring/diagram/action/popup/ShowAllTypeDependencies.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/popup/ShowAllTypeDependencies.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.gmf.runtime.common.ui.action.AbstractActionDelegate;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramRootEditPart;
 import org.eclipse.ui.IObjectActionDelegate;
 
+import reflexactoring.Reflexactoring;
+import reflexactoring.TypeDependency;
 import reflexactoring.diagram.action.DiagramUpdater;
 import reflexactoring.diagram.bean.ModuleWrapper;
+import reflexactoring.diagram.edit.parts.TypeDependencyEditPart;
+import reflexactoring.diagram.util.GEFDiagramUtil;
 import reflexactoring.diagram.util.ReflexactoringUtil;
 import reflexactoring.diagram.util.Settings;
 
@@ -20,7 +25,15 @@ public class ShowAllTypeDependencies  extends AbstractActionDelegate implements 
 	@Override
 	protected void doRun(IProgressMonitor progressMonitor) {
 		Settings.enableVisibility = true;
-		ArrayList<ModuleWrapper> moduleList = ReflexactoringUtil.getModuleList(Settings.diagramPath);
-		new DiagramUpdater().generateReflexionModel(moduleList, Settings.scope.getScopeCompilationUnitList());
+		
+		DiagramRootEditPart diagramRoot = GEFDiagramUtil.getRootEditPart();
+		
+		Reflexactoring reflexactoring = GEFDiagramUtil.findReflexactoring(diagramRoot);
+		for(TypeDependency dep: reflexactoring.getTypeDependencies()){
+			TypeDependencyEditPart part = GEFDiagramUtil.findCorrespondingTypeDependencyEditPart(diagramRoot, dep);
+			part.getFigure().setVisible(true);
+		}
+		//ArrayList<ModuleWrapper> moduleList = ReflexactoringUtil.getModuleList(Settings.diagramPath);
+		//new DiagramUpdater().generateReflexionModel(moduleList, Settings.scope.getScopeCompilationUnitList());
 	}
 }
