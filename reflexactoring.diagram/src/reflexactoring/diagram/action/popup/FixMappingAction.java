26ac60a30a0813120991fad83cabb83ae510ed0a
linyun <llmhyy@gmail.com>
2014-08-30 22:25:21 +0800

    A framework for Adi

---
+++ reflexactoring.diagram/src/reflexactoring/diagram/action/popup/FixMappingAction.java
@@ -0,0 +1,18 @@
+package reflexactoring.diagram.action.popup;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gmf.runtime.common.ui.action.AbstractActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+
+public class FixMappingAction extends AbstractActionDelegate implements IObjectActionDelegate{
+
+	public FixMappingAction() {
+	}
+
+	@Override
+	protected void doRun(IProgressMonitor progressMonitor) {
+		// TODO for Adi
+		System.out.println("test");
+	}
+
+}
