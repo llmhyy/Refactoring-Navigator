323ca3e19c0adf9c0a35b010f3dcadc404194d71
xxx_di <xxx_di@qq.com>
2014-08-31 15:49:48 +0800

    reset the default value of settings.

--- reflexactoring.diagram/src/reflexactoring/diagram/util/Settings.java
+++ reflexactoring.diagram/src/reflexactoring/diagram/util/Settings.java
@@ -135,9 +135,9 @@ public class Settings {
 	public static double alpha = 0.5;
 	public static double beta = 0.5;
 	public static double mutationRate = 0.01;
-	public static int mappingIterationNum = 1000;
-	public static int climbIterationNum = 1000;
-	public static int populationSize = 50;
+	public static int mappingIterationNum = 500;
+	public static int climbIterationNum = 20;
+	public static int populationSize = 100;
 	public static int suggestionNum = 3;
 	
 	//=============================================================
